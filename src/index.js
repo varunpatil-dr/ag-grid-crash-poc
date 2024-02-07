71c46aea7ba4063ff65a1bc4ed196d2feb1c10a5
Varun Patil <varun.patil@devrev.ai>
2024-02-08 00:36:17 +0530

    base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import { AgGridReact } from "@ag-grid-community/react"; // React Grid Logic
+import "@ag-grid-community/styles/ag-grid.css"; // Core CSS
+import "@ag-grid-community/styles/ag-theme-alpine.css"; // Theme
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+const GridExample = () => {
+  const rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ];
+
+  const colDefs = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ];
+
+  const [rowClass, setRowClass] = useState("");
+
+  return (
+    <div
+      className="ag-theme-alpine-dark"
+      style={{ width: "1000px", height: "1000px" }}
+    >
+      <button onClick={() => setRowClass("xyz")}>Click to crash</button>
+      <AgGridReact rowData={rowData} columnDefs={colDefs} rowClass={rowClass} />
+    </div>
+  );
+};
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(<GridExample />);
